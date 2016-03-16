8be13bbbb8cf6b098985fdbb01eb427c7bc0c21f
Seth Yastrov <syastrov@gmail.com>
2016-03-16 10:58:06 +0100

    Get things working.

---
+++ README.md
@@ -0,0 +1,6 @@
+Alfresco Share module to hide the "Download Original" button from the Quickshare page
+=====================================================================================
+
+The title pretty much explains it all.
+
+Use `mvn package` to build an AMP file in the target directory.
--- src/main/amp/web/dk/magenta/share-hide-download-original/hide.js
+++ src/main/amp/web/dk/magenta/share-hide-download-original/hide.js
@@ -1,12 +1,34 @@
 // Check if we are on the quickshare page
 if (window.location.pathname.indexOf(Alfresco.constants.QUICKSHARE_URL.replace("{sharedId}", "")) !== -1) {
-  var preview = Alfresco.util.ComponentManager.findFirst("Alfresco.WebPreview");
-  if (preview != null && preview.plugin != null && preview.plugin instanceof Alfresco.WebPreview.prototype.Plugins.PdfJs) {
-    var plugin = preview.plugin;
-    items = plugin.widgets.downloadButton.getMenu().getItems();
-    if (items.length == 2) {
-      // Remove the first item from the menu if there are two items.
-      plugin.widgets.downloadButton.getMenu().removeItem(items[0], 0);
+  // Keep checking until the web preview component is loaded
+  var intervalId = setInterval(function () {
+    var preview = Alfresco.util.ComponentManager.findFirst("Alfresco.WebPreview");
+    if (preview != null && preview.plugin != null) {
+      if (preview.plugin instanceof Alfresco.WebPreview.prototype.Plugins.PdfJs) {
+        var plugin = preview.plugin;
+        if (!plugin.widgets.downloadButton) {
+          return;
+        }
+        // Get rid of the menu
+        plugin.widgets.downloadButton._setMenu([]);
+
+        // Choose the onClick based on whether this is a PDF thumbnail or not.
+        var onClick = plugin.attributes.src ? Alfresco.WebPreview.prototype.Plugins.PdfJs.prototype.onDownloadPDFClick : Alfresco.WebPreview.prototype.Plugins.PdfJs.prototype.onDownloadClick;
+
+        // Set the title of the button to "Download PDF"
+        // Note that we have to include the icon here as setting the label
+        // clears the innerHTML.
+        plugin.widgets.downloadButton._setLabel('<img src="' + Alfresco.constants.URL_CONTEXT + 'res/components/documentlibrary/actions/document-download-16.png" align="top" height="16"/> ' + plugin.wp.msg("link.downloadPdf"));
+
+        // Add a click handler
+        plugin.widgets.downloadButton._setOnClick({
+          fn: onClick,
+          scope: plugin
+        });
+        clearInterval(intervalId);
+      } else {
+        clearInterval(intervalId);
+      }
     }
-  }
+  }, 50);
 }
